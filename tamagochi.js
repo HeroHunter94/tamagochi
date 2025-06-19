d6cef1e79ca580caef62d706abc1315cc30b85e0
HeroHunter94 <danilorusso.info94@gmail.com>
2025-06-20 00:19:01 +0200

    Add files via upload

---
+++ tamagochi.js
@@ -0,0 +1,13 @@
+const ROUTE = "./";
+const health = ["health0.png", "health1.png", "health2.png", "health3.png", "health4.png"];
+const clean = ["clean0.png", "clean1.png", "clean2.png", "clean3.png", "clean4.png"];
+const happiness = ["happiness0.png", "happiness1.png", "happiness2.png", "happiness3.png", "happiness4.png"];
+const energy = ["energy0.png", "energy1.png", "energy2.png", "energy3.png", "energy4.png"];
+const statTama = 
+    {
+        name : "Kiddo",
+        health : 0,
+        clean : 0,
+        happiness : 0,
+        energy: 0,
+    }
